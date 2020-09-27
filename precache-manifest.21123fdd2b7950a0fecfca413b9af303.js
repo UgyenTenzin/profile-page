6bcd361c948660cc19ea1e535e82bf5307eb3ad5
Ugyen Tenzin <ugyen10z@gmail.com>
2020-09-27 21:40:17 +0800

    Updates

---
+++ precache-manifest.21123fdd2b7950a0fecfca413b9af303.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8a56a11a81b4bca1f69ec391126d02",
+    "url": "/profile-page/index.html"
+  },
+  {
+    "revision": "235df1b685828822f5bf",
+    "url": "/profile-page/static/css/main.43db5622.chunk.css"
+  },
+  {
+    "revision": "cfaa947e11194296cccf",
+    "url": "/profile-page/static/js/2.d47fab1c.chunk.js"
+  },
+  {
+    "revision": "235df1b685828822f5bf",
+    "url": "/profile-page/static/js/main.3b44e082.chunk.js"
+  },
+  {
+    "revision": "9800e6dfedf813eee1be",
+    "url": "/profile-page/static/js/runtime-main.cba83292.js"
+  }
+]);
