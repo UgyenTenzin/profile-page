0161d20bdfc833abd65764ed89c4cec1eb617487
Ugyen Tenzin <ugyen10z@gmail.com>
2020-09-27 22:24:34 +0800

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Me from './Me';
-import Projects from './Projects';
-import Work from './Work';
-import Education from './Education';
+import BrowserRouter from "react-router-dom/BrowserRouter";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Me from "./Me";
+import Projects from "./Projects";
+import Work from "./Work";
+import Education from "./Education";
 
 const Pages = ({ user }) => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Me user={user} />
-        </Route>
-        <Route path="/projects">
-          <Projects user={user} />
-        </Route>
-        <Route path="/work">
-          <Work user={user} />
-        </Route>
-        <Route path="/education">
-          <Education user={user} />
-        </Route>
-      </Switch>
-    </Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Me user={user} />
+          </Route>
+          <Route path="/projects">
+            <Projects user={user} />
+          </Route>
+          <Route path="/work">
+            <Work user={user} />
+          </Route>
+          <Route path="/education">
+            <Education user={user} />
+          </Route>
+        </Switch>
+      </Router>
+    </BrowserRouter>
   );
 };
 
